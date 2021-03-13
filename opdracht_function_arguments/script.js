534a840d764c702f18afccf71f32494295e1afe6
Svt1406 <79196867+Svt1406@users.noreply.github.com>
2021-03-13 02:05:46 +0100

    Add files via upload

---
+++ opdracht_function_arguments/script.js
@@ -0,0 +1,8 @@
+const paintWalls = function (location, color){
+    console.log(location + ' has been painted ' + color);
+}
+
+// paintWalls ('green');
+// paintWalls ('pink');
+paintWalls ('the north wall', 'green');
+paintWalls ('the south-east wall', 'pink');
